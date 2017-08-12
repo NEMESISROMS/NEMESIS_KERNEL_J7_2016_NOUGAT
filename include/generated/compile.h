a6f4278810067acd54ad8797dccd86ef1fbf0ae3
geiti94 <geiti94@me.com>
2017-08-12 12:51:12 -0700

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 28 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#28 SMP PREEMPT Sat Aug 12 12:45:35 PDT 2017"
+#define LINUX_COMPILE_BY "geiti94"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
