85ac990862144c852877468f5b0a03e4cc47a2b1
geiti94 <geiti94@me.com>
2017-08-15 03:09:31 -0700

    Push missing J7 Prime G610F stuff

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 35 */
+/* This file is auto generated, version 36 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#35 SMP PREEMPT Sun Aug 13 07:37:05 PDT 2017"
+#define UTS_VERSION "#36 SMP PREEMPT Mon Aug 14 03:34:18 PDT 2017"
 #define LINUX_COMPILE_BY "geiti94"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
